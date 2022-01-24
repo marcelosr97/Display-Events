db0d730cd32ca6d3bd6f6269399d026ae4d8cffc
marcelosr97 <marcelo_sr97@hotmail.com>
2022-01-24 18:11:33 +0100

    SAE Library
    Images are now print in colour.
    Executable was updated.

--- SAE/sae.h
+++ SAE/sae.h
@@ -12,8 +12,8 @@ class SAE{
         void ShowEvents(uint16_t x, uint16_t y, int16_t p, int64_t timestamp);
     private:
         /* Images to show */
-        cv::Mat sae_pos; // create the grayscale image for positive sae
-        cv::Mat sae_neg; // create the grayscale image for negative sae
+        cv::Mat sae_pos; 
+        cv::Mat sae_neg; 
         /* Variables */
         int64_t elapsed_t = 0;
         int64_t recording_duration = 0;
@@ -21,11 +21,12 @@ class SAE{
 
 template<int32_t width, int32_t height, int64_t delta_t>
 SAE<width, height, delta_t>::SAE(void){
-    cv::Mat tmp_sae_pos(height, width, CV_8UC1, cv::Scalar(0)); // create the grayscale image for positive sae
-    cv::Mat tmp_sae_neg(height, width, CV_8UC1, cv::Scalar(0)); // create the grayscale image for negative sae
+    /* Tmp RGB images and asignation */
+    cv::Mat tmp_sae_pos(height, width, CV_8UC3, cv::Scalar(0,0,0)); 
+    cv::Mat tmp_sae_neg(height, width, CV_8UC3, cv::Scalar(0,0,0)); 
     sae_pos = tmp_sae_pos;
     sae_neg = tmp_sae_neg;
-    // show first image
+    /* Show first image */
     cv::imshow("Positive SAE", sae_pos);    
     cv::imshow("Negative SAE", sae_neg);    
 }
@@ -37,20 +38,27 @@ void SAE<width, height, delta_t>::ShowEvents(uint16_t x, uint16_t y, int16_t p, 
         uint8_t colour = 0;
         colour = (uint8_t)((timestamp - elapsed_t) * 255/delta_t);
 
-        // y * width * (nº canales) + x * (nº canales) + canal
+        /* y * width * max channels + x * max channels + channel */
         if(p) {
-            sae_pos.data[y*width + x] = (int)colour;
+            sae_pos.data[y*width*3 + x*3] = 255 - (int)colour; /* RED CHANNEL */
+            sae_pos.data[y*width*3 + x*3 + 1] = (int)0; /* GREEN CHANNEL */
+            sae_pos.data[y*width*3 + x*3 + 2] = (int)colour; /* BLUE CHANNEL */
+
         }
         else {
-            sae_neg.data[y*width + x] = (int)colour;
+            sae_neg.data[y*width*3 + x*3] = 255 - (int)colour; /* RED CHANNEL */
+            sae_neg.data[y*width*3 + x*3 + 1] = (int)0; /* GREEN CHANNEL */
+            sae_neg.data[y*width*3 + x*3 + 2] = (int)colour; /* BLUE CHANNEL */        
         }
 
-        // check if image is ready
+        /* Check if an image must be printed */
         if(timestamp - elapsed_t > delta_t) {
-            cv::imshow("Positive SAE", sae_pos); //print the image
-            cv::imshow("Negative SAE", sae_neg); //print the image
-            memset(sae_pos.data, 0, (height-1)*width*1 + (width-1)*1); //reset the image
-            memset(sae_neg.data, 0, (height-1)*width*1 + (width-1)*1); //reset the image
+            /* Print images */
+            cv::imshow("Positive SAE", sae_pos); 
+            cv::imshow("Negative SAE", sae_neg); 
+            /* Reset images */
+            memset(sae_pos.data, 0, (height-1)*width*3 + (width-1)*3+2); 
+            memset(sae_neg.data, 0, (height-1)*width*3 + (width-1)*3+2);
             elapsed_t = timestamp;
         }
 }
