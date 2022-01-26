614555854a5abcdda7a087a4a1b92dc8e33e37a3
marcelosr97 <marcelo_sr97@hotmail.com>
2022-01-26 08:19:11 +0100

    Absolute SAE
    Absolute SAE library and executable.

---
+++ AbsoluteSAE/absolutesae.h
@@ -0,0 +1,50 @@
+#include <opencv2/opencv.hpp>
+#include <inttypes.h>
+
+using namespace std;
+
+template<int32_t width, int32_t height, int64_t delta_t>
+class AbsoluteSAE{
+    public:
+        /* Constructor */
+        AbsoluteSAE();
+        /* Show Events */
+        void ShowEvents(uint16_t x, uint16_t y, int16_t p, int64_t timestamp);
+    private:
+        /* Images to show */
+        cv::Mat sae;
+        /* Variables */
+        int64_t elapsed_t = 0;
+        int64_t recording_duration = 0;
+};
+
+template<int32_t width, int32_t height, int64_t delta_t>
+AbsoluteSAE<width, height, delta_t>::AbsoluteSAE(void){
+    /* Tmp RGB images and asignation */
+    cv::Mat tmp_sae(height, width, CV_8UC3, cv::Scalar(0,0,0)); 
+    sae = tmp_sae;
+    /* Show first image */
+    cv::imshow("SAE", sae);    
+}
+
+template<int32_t width, int32_t height, int64_t delta_t>
+void AbsoluteSAE<width, height, delta_t>::ShowEvents(uint16_t x, uint16_t y, int16_t p, int64_t timestamp){
+
+        /* Colour of the pixel */
+        uint8_t colour = 0;
+        colour = (uint8_t)((timestamp - elapsed_t) * 255/delta_t);
+
+        /* y * width * max channels + x * max channels + channel */
+        sae.data[y*width*3 + x*3] = 255 - (int)colour; /* RED CHANNEL */
+        sae.data[y*width*3 + x*3 + 1] = (int)0; /* GREEN CHANNEL */
+        sae.data[y*width*3 + x*3 + 2] = (int)colour; /* BLUE CHANNEL */
+
+        /* Check if an image must be printed */
+        if(timestamp - elapsed_t > delta_t) {
+            /* Print images */
+            cv::imshow("SAE", sae); 
+            /* Reset images */
+            memset(sae.data, 0, (height-1)*width*3 + (width-1)*3+2); 
+            elapsed_t = timestamp;
+        }
+}
